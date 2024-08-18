315b80fac9831a0b7e7b664076a002875f726c70
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2024-08-18 20:57:27 +0300

    add font script

---
+++ font.sh
@@ -0,0 +1,5 @@
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+mkdir ~/.local/share/fonts 
+mkdir ~/.local/share/fonts/JetBrains
+unzip JetBrainsMono.zip -d ~/.local/share/fonts/JetBrains
+rm JetBrainsMono.zip
