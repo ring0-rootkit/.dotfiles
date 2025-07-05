0b27a3af946aca9164069691361ada73e71d545d
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-07-05 12:38:43 +0300

    updates yesesessee| g

--- .zshrc
+++ .zshrc
@@ -90,3 +90,5 @@ export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 export CC=clang
+
+source ~/.aliases
--- font.sh
+++ font.sh
@@ -1,9 +1,9 @@
-#wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
 wget https://dtinth.github.io/comic-mono-font/ComicMono.ttf
 mkdir ~/.local/share/fonts 
-#mkdir ~/.local/share/fonts/JetBrains
+mkdir ~/.local/share/fonts/JetBrains
 mkdir ~/.local/share/fonts/ComicMono
 cp ./ComicMono.ttf ~/.local/share/fonts/ComicMono/
-#unzip JetBrainsMono.zip -d ~/.local/share/fonts/JetBrains
-#rm JetBrainsMono.zip
+unzip JetBrainsMono.zip -d ~/.local/share/fonts/JetBrains
+rm JetBrainsMono.zip
 rm ComicMono.ttf
