7d2978022ba474f57fc2fed73c80f02c14de98bd
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-07-17 22:40:42 +0300

    moving to catppuccin dark

--- .config/nvim/lua/ring0/plugins/theme.lua
+++ .config/nvim/lua/ring0/plugins/theme.lua
@@ -1,19 +1,19 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			variant = "auto", -- auto, main, moon, or dawn
-			dark_variant = "main", -- main, moon, or dawn
-
-			styles = {
-				transparency = true,
-			},
-			enable = {
-				legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
-			},
-		})
-
-		vim.cmd("colorscheme rose-pine")
-	end,
+	{
+		"blazkowolf/gruber-darker.nvim",
+	},
+	{
+		"catppuccin/nvim",
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
+		end,
+	},
+	{
+		"theme",
+		init = function()
+			vim.cmd("colorscheme catppuccin-mocha")
+		end,
+	},
 }
