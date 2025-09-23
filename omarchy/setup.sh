b52c70fcf46c17f1536156abfe63f9e9cd819bc8
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-09-23 22:27:21 +0300

    omarchy: hypr: mouse flat accel

--- omarchy/setup.sh
+++ omarchy/setup.sh
@@ -1,5 +1,8 @@
 rm ~/.config/hypr/bindings.conf
+rm ~/.config/hypr/input.conf
 ln -s $(pwd)/hypr/bindings.conf ~/.config/hypr/bindings.conf
+ln -s $(pwd)/hypr/input.conf ~/.config/hypr/input.conf
+
 ln -s $(pwd)/../.zshrc ~/.zshrc
 ln -s $(pwd)/../.aliases ~/.aliases
 
