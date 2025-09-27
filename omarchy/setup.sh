942167bf79f506eb301e445cc9087043d1fbe0d0
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-09-27 20:57:51 +0300

    add monitors to omarchy

--- omarchy/setup.sh
+++ omarchy/setup.sh
@@ -1,7 +1,10 @@
 rm ~/.config/hypr/bindings.conf
 rm ~/.config/hypr/input.conf
+rm ~/.config/hypr/monitors.conf
+
 ln -s $(pwd)/hypr/bindings.conf ~/.config/hypr/bindings.conf
 ln -s $(pwd)/hypr/input.conf ~/.config/hypr/input.conf
+ln -s $(pwd)/hypr/monitors.conf ~/.config/hypr/monitors.conf
 
 ln -s $(pwd)/../.zshrc ~/.zshrc
 ln -s $(pwd)/../.aliases ~/.aliases
