f8b875d1c6c25a36fa2a0e310f1ab2fd09f3b662
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-10-19 13:51:15 +0300

    update waybar

--- omarchy/setup.sh
+++ omarchy/setup.sh
@@ -1,6 +1,7 @@
 rm ~/.config/hypr/bindings.conf
 rm ~/.config/hypr/input.conf
 rm ~/.config/hypr/monitors.conf
+rm ~/.config/hypr/looknfeel.conf
 
 rm ~/.config/waybar/style.css
 rm ~/.config/waybar/config.jsonc
@@ -8,6 +9,7 @@ rm ~/.config/waybar/config.jsonc
 ln -s $(pwd)/hypr/bindings.conf ~/.config/hypr/bindings.conf
 ln -s $(pwd)/hypr/input.conf ~/.config/hypr/input.conf
 ln -s $(pwd)/hypr/monitors.conf ~/.config/hypr/monitors.conf
+ln -s $(pwd)/hypr/looknfeel.conf ~/.config/hypr/looknfeel.conf
 
 ln -s $(pwd)/waybar/style.css ~/.config/waybar/style.css
 ln -s $(pwd)/waybar/config.jsonc ~/.config/waybar/config.jsonc
