20925363cc506484c2c2a2bc8c3448b27f6f252a
ring0-rootkit <ord1naryman.dmitry@gmail.com>
2025-10-25 15:18:21 +0300

    move fedora setup back to gnome?

--- scripts/54_cursor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp cursor.desktop ~/.local/share/applications
-desktop-file-install --dir="~/.local/share/applications" cursor.desktop
-update-desktop-database ~/.local/share/applications
